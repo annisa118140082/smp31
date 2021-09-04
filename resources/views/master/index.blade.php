f43bdc8623f2d4f7b95706aeebb0faa963656cbd
annisa118140082 <annisa.118140082@student.itera.ac.id>
2021-09-04 17:21:56 +0700

    init laravel

---
+++ resources/views/master/index.blade.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/master/master.css') }}" rel="stylesheet">
+    <title>SMP Negeri 31 BDL</title>
+</head>
+
+<body>
+    <div class="wrap d-flex justify-content-center flex-column">
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+                integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+                integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+                integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
+        <script src="{{ asset('js/master.js') }}"></script>
+
+        <nav class="navbar navbar-expand-lg navbar-light navbar-transparent position-sticky sticky-top navbar-fixed-top">
+            <a class="navbar-brand d-flex" style="flex: 2" href="#">Lorem, ipsum.</a>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav d-flex flex-row justify-content-center" style="flex: 1">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarProfile" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Profil Sekolah</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarProfile">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#berita">Berita</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ekstrakulikuler</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Prestasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Profil Alumni</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Kontak</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="landing-text d-flex flex-column justify-content-center align-items-center">
+            <div class="big-text mb-3">
+                SMP NEGERI 31 BANDAR LAMPUNG
+            </div>
+            <div>
+                <form class="form-control form-control-lg d-flex flex-row">
+                    <input class="form-control mr-sm-2 pr-5" type="search" placeholder="Cari informasi">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Cari</button>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    <main>
+        <div class="content">
+            <article id="berita">
+                <div class="title">
+                    Berita
+                </div>
+                <div class="bar"></div>
+                <div class="card-deck">
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to
+                                additional content.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src="..." alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This card has even longer content than the first to show
+                                that equal height action.</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </main>
+    <footer>
+        <div class="mapouter">
+            <div class="gmap_canvas">
+                <iframe width="500" height="500" id="gmap_canvas"
+                    src="https://maps.google.com/maps?q=smpn%2031%20bandar%20lampung&t=&z=15&ie=UTF8&iwloc=&output=embed"
+                    frameborder="0" scrolling="no" marginheight="0" marginwidth="0">
+                </iframe>
+                <a href="https://getasearch.com/nordvpn-coupon/"></a><br>
+                <style>
+                    .mapouter {
+                        position: relative;
+                        text-align: right;
+                        height: 500px;
+                        width: 500px;
+                    }
+
+                </style>
+                <a href="https://www.embedgooglemap.net">embedgooglemap.net</a>
+                <style>
+                    .gmap_canvas {
+                        overflow: hidden;
+                        background: none !important;
+                        height: 500px;
+                        width: 500px;
+                    }
+
+                </style>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
